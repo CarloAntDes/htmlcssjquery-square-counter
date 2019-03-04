7a977cac13ee3fdb9133a88b8b7a42ccc0ba8579
CarloAntDes <carloantoniodesantis@gmail.com>
2019-03-04 15:41:52 +0100

    Completo ma da rivedere

--- main.js
+++ main.js
@@ -4,3 +4,42 @@ Il quadrato centrale inizialmente avrà uno 0 al centro
 Il quadrato di sinistra DECREMENTERÀ il valore del quadrato centrale a ogni click
 Il quadrato di destra INCREMENTERÀ il valore del quadrato centrale a ogni click
 */
+
+//Prima versione con variabile globale
+
+// var contatore = 0;
+//
+// $(".quadrato_left").click(function() {
+//   contatore--;
+//   console.log(contatore);
+//   $(".quadrato_center").html(contatore)
+//
+// });
+//
+// $(".quadrato_right").click(function() {
+//   contatore++;
+//   console.log(contatore);
+//   $(".quadrato_center").html(contatore)
+//
+// });
+
+//Versione senza variabile globale
+
+$(".quadrato_left").click(function() {
+  var risultato =  $(".quadrato_center").html()
+  console.log(risultato);
+  risultato = parseInt(risultato);
+  risultato--;
+  $(".quadrato_center").html(risultato);
+});
+
+$(".quadrato_right").click(function() {
+  var risultato =  $(".quadrato_center").html()
+  console.log(risultato);
+  risultato = parseInt(risultato);
+  risultato++;
+  $(".quadrato_center").html(risultato);
+
+});
+
+//Manca sintetizzare il codice
