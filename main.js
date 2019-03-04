40f022c93b47a630cf4a8f0ad6733b0694dbd7ff
CarloAntDes <carloantoniodesantis@gmail.com>
2019-03-04 14:48:19 +0100

    Start

---
+++ main.js
@@ -0,0 +1,6 @@
+/*
+Creare 3 quadrati di 70px messi uno fianco all'altro di 3 colori diversi
+Il quadrato centrale inizialmente avrà uno 0 al centro
+Il quadrato di sinistra DECREMENTERÀ il valore del quadrato centrale a ogni click
+Il quadrato di destra INCREMENTERÀ il valore del quadrato centrale a ogni click
+*/
